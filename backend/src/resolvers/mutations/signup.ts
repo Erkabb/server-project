6e9418a5a2eeff791fad9207c98a452ef812073c
erkabb <erkabb816@gmail.com>
2025-11-30 18:38:56 +0800

    Add support for querying product by track code in schemas and resolvers
    This commit introduces the `getProductByTrackCode` query, enabling products to be fetched by their `trackCode`. Necessary updates have been made to resolvers, GraphQL types, and schemas for consistency.

--- backend/src/resolvers/mutations/signup.ts
+++ backend/src/resolvers/mutations/signup.ts
@@ -1,18 +1,12 @@
 import { MutationResolvers } from "@/generated/graphql";
 import User from "../../models/auth.model";
 
-export const signUp: MutationResolvers["signUp"] = async (
-  _,
-  { input },
-  { websiteId: authWebsiteId },
-) => {
+export const signUp: MutationResolvers["signUp"] = async (_, { input }) => {
   const { firstname, lastname, email, password, websiteId } = input;
 
   const findUser = await User.findOne({ email, websiteId });
 
   if (findUser) throw new Error("User already signed up.");
-
-  if (websiteId !== authWebsiteId) throw new Error("Invalid website access");
 
   return await User.create({
     firstname,
